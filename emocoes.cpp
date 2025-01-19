7fa09a7b762c29d2ab93d9098b559531c0e3153e
cardoso <jefersoncardoso.dev@gmail.com>
2025-01-19 01:44:35 -0400

    prototipo de classe
    Prototipo de classes

--- emocoes.cpp
+++ emocoes.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include "emocoes.hh"
 
+using namespace std;
+typedef std::string msg;
+
+template <class T>
+AnalisadorSentimento<T>::AnalisadorSentimento(T frase) {
+    T palavras[20];
+    int i = 0;
+    T palavra;
+    int pontuacao_avaliacao = 0;
+
+    for (int j = 0; j < frase.length(); j++) {
+        if (frase[j] == ' ') {
+            palavras[i] = palavra;
+            i++;
+            palavra = "";
+        } else {
+            palavra += frase[j];
+        }
+    }
+    palavras[i] = palavra;
+
+    for (int k = 0; k <= i; k++) {
+        if (find(begin(palavras_positivas), end(palavras_positivas), palavras[k]) != end(palavras_positivas))
+            pontuacao_avaliacao += 1;
+        else if (find(begin(palavras_negativas), end(palavras_negativas), palavras[k]) != end(palavras_negativas))
+            pontuacao_avaliacao -= 1;
+    }
+
+    avaliacao(pontuacao_avaliacao);
+}
