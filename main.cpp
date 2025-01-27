c5e38cb669fe444fb3649a9825049208262d2335
cardoso <jefersoncardoso.dev@gmail.com>
2025-01-27 08:41:49 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "emocoes.hh"
+#include <string>
+
+int main(int argc, char **argv){  
+typedef std::string str;
+
+new AnalisadorSentimento<str>("Estou me sentindo triste e desanimado");
+
+    return EXIT_SUCCESS;
+}
+
